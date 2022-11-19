ceaefed09a9ac595c777205869b785c41cad3f7a
KrisN9 <kristinank992@gmail.com>
2022-11-19 18:04:11 +0000

    conectar función contador con divs

--- src/js/component/CounterInterval.js
+++ src/js/component/CounterInterval.js
@@ -4,7 +4,19 @@ let counter = 0;
 
 let CounterInterval = () => {
     setInterval(function() {
-
+        const six = Math.floor(counter/100000);
+        const five = Math.floor(counter/10000);
+        const four = Math.floor(counter/1000);
+        const three = Math.floor(counter/100);
+        const two = Math.floor(counter/10);
+        const one = Math.floor(counter/1);
+        counter++;
+        oneDigit={one};
+        twoDigit={two};
+        threeDigit={three};
+        fourDigit={four};
+        fiveDigit={five};
+        sixDigit={six};
     }, 1000);
 }
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import CounterInterval from "./CounterInterval";
 
 //create your first component
 const Home = (props) => {
 	return (
 		<>
+		<CounterInterval />
 		<div className="container bg-dark text-light d-flex justify-content-center">
 			<div className="row">
 				<p><i className="bi bi-clock"></i></p>
 			</div>
-			<div id="six-digit">{props.sixDigit}</div>
-			<div id="five-digit">{props.fiveDigit}</div>
-			<div id="four-digit">{props.fourDigit}</div>
-			<div id="three-digit">{props.threeDigit}</div>
-			<div id="two-digit">{props.twoDigit}</div>
-			<div id="one-digit">{props.oneDigit}</div>
+			<div id="six-digit">{props.sixDigit % 10}</div>
+			<div id="five-digit">{props.fiveDigit % 10}</div>
+			<div id="four-digit">{props.fourDigit % 10}</div>
+			<div id="three-digit">{props.threeDigit % 10}</div>
+			<div id="two-digit">{props.twoDigit % 10}</div>
+			<div id="one-digit">{props.oneDigit % 10}</div>
 		</div>
 		</>
 	);
