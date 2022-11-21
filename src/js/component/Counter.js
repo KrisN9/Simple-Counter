3e97f0c1f9e0c79b8caa2f2abc67db347f104904
KrisN9 <kristinank992@gmail.com>
2022-11-21 02:56:54 +0000

    errores en el ejercicio

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -1,18 +1,18 @@
 import React from "react";
+import { ProgressPlugin } from "webpack";
 
 let counter = 0;
 
 let Counter = () => {
     setInterval(function() {
         counter++;
-        oneDigit= Math.floor(counter/1);
-        twoDigit= Math.floor(counter/10);
-        threeDigit= Math.floor(counter/100);
-        fourDigit= Math.floor(counter/1000);
-        fiveDigit= Math.floor(counter/10000);
-        sixDigit= Math.floor(counter/100000);
+        const one = Math.floor(counter/1);
+        const two = Math.floor(counter/10);
+        const three = Math.floor(counter/100);
+        const four = Math.floor(counter/1000);
+        const five = Math.floor(counter/10000);
+        const six = Math.floor(counter/100000);
     }, 1000);
 }
-
 
 export default Counter
